1d1beec4308cba4712906d8e9586a39b005fadaa
Ümit Ahmed <135972028+umittahmett@users.noreply.github.com>
2025-04-25 12:08:17 +0300

    refactor: remove JotaiProvider and add background image to dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import { AppSidebar } from "@/components/navigation/app-sidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { Provider as JotaiProvider } from "jotai";
+import WavesBackground from "@assets/images/waves-background.webp";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -9,15 +10,21 @@ export default function RootLayout({
 }>) {
   return (
     <div>
-      <JotaiProvider>
-        <SidebarProvider>
-          <AppSidebar />
-          <main>
-            <SidebarTrigger />
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="relative w-full overflow-hidden max-h-dvh p-6 lg:p-8 !pb-0 bg-repeat bg-cover ">
+          <SidebarTrigger className="relative z-10" />
+          <div className="relative z-10 max-h-full mx-auto scrollbar-hidden overflow-y-auto max-w-7xl">
             {children}
-          </main>
-        </SidebarProvider>
-      </JotaiProvider>
+          </div>
+          {/* Background */}
+          <Image
+            className="size-full -z-1- absolute inset-0"
+            src={WavesBackground}
+            alt="auth-background"
+          />
+        </main>
+      </SidebarProvider>
     </div>
   );
 }
