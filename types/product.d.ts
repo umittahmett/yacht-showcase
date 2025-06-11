0583ec03f18473d2988aa1196863b2757858af80
Ümit Ahmed <135972028+umittahmett@users.noreply.github.com>
2025-06-12 02:00:55 +0300

    feat: add TypeScript interfaces for pricing, features, groups, and languages

---
+++ types/product.d.ts
@@ -0,0 +1,64 @@
+export interface PricingType {
+  id: number;
+  field_name: string;
+  created_at: string;
+  name?:string
+  language_code?:string
+}
+
+export interface FeatureTranslation {
+  id: number;
+  feature_id: number;
+  name: string;
+  language_code: string;
+  created_at: string;
+  feature_name?: string;
+}
+
+export interface PricingPeriod {
+  id: number;
+  field_name: string;
+  field_type: string;
+  required: boolean;
+  localizable: boolean; 
+  created_at: string;
+}
+
+export interface Group {
+  title: string;
+  group_language_code?:string;
+  fields: GroupField[]
+}
+
+export interface GroupField {
+  id: number;
+  field_title?:string;
+  field_language_code?:string
+  name: string;
+  field_name: string;
+  field_type: string;
+  required: boolean;
+  localizable: boolean;
+  language_code: string;
+  created_at: string;
+}
+
+export interface GroupTranslationField {
+  id: number;
+  feature_id: number;
+  name: string;
+  language_code: string;
+  created_at: string;
+}
+
+export interface Language {
+  id: number;
+  code: string;
+  name: string;
+  created_at: string;
+}
+
+export interface GroupTranslation {
+  title: string;
+  fields: GroupTranslationField[]
+}
