6c7062501493eafb79fe9ce94f3313be558b0d77
Ümit Ahmed <135972028+umittahmett@users.noreply.github.com>
2025-07-28 01:27:23 +0300

    refactor: streamline global styles and enhance button, input, and label components with new variants and improved responsiveness

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,19 +1,36 @@
 import * as React from "react";
-
+import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+const inputVariants = cva(
+  "placeholder:capitalize outline-none ring-0 duration-200 transition-all py-3 flex disabled:cursor-not-allowed disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default:
+          "rounded-[10px] bg-main-background text-primary placeholder:text-gray-100",
+      },
+      size: {
+        default: "h-[60px] px-[30px]",
+        sm: "h-[46px] px-[30px] text-xs",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+function Input({ className, type, variant, size, ...props }: React.ComponentProps<"input"> & VariantProps<typeof inputVariants>) {
   return (
     <input
       type={type}
       data-slot="input"
-      className={cn(
-        "h-10 w-full text-sm placeholder:capitalize text-neutral-900 border border-gray-300 px-4 outline-none ring-0 focus:ring-2 focus:ring-sky-600 duration-200 transition-all py-3 flex rounded-lg bg-transparent shadow-sm disabled:cursor-not-allowed disabled:opacity-50",
-        className
-      )}
+      className={cn(inputVariants({ variant, size, className }))}
       {...props}
     />
   );
 }
 
-export { Input };
+export { Input, inputVariants };
