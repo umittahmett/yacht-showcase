b19715d8607287db202232feb6e18cebd1b2dc7a
Ümit Ahmed <135972028+umittahmett@users.noreply.github.com>
2025-07-29 23:35:13 +0300

    fix: add maximum width to highlight video for better visibility in large screens

--- components/section/highlight-video.tsx
+++ components/section/highlight-video.tsx
@@ -5,7 +5,7 @@ import { HighLightVideoProps } from "@/types";
 
 const HighLightVideo:React.FC<HighLightVideoProps> = ({videoSrc, thumbnailAlt, thumbnailSrc})=>{
   return (
-    <div className="relative">
+    <div className="relative max-w-[1920px] max-[1920px]:rounded-none overflow-hidden rounded-[20px] mx-auto w-full">
       <HeroVideoDialog
         animationStyle="from-center"
         videoSrc={videoSrc}
